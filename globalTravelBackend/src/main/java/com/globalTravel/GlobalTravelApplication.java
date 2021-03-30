60edfdb47509c854530a00c1da3ea53a4b7b0aaa
Zero <razviy69@gmail.com>
2021-03-30 10:38:06 +0200

    Initial commit

---
+++ globalTravelBackend/src/main/java/com/globalTravel/GlobalTravelApplication.java
@@ -0,0 +1,13 @@
+package com.globalTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTravelApplication.class, args);
+	}
+
+}
---
+++ globalTravelBackend/src/test/java/com/globalTravel/GlobalTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
